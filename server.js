0d629258bb1783bcde8ea54028cad54a2bfe2edb
hiendinhngoc <hiendn@bitnemo.vn>
2016-01-18 23:50:36 +0700

    :tv: complete express api routes

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -79,6 +79,32 @@ app.listen(app.get('port'), function(){
   console.log('Express server listening on port' + app.get('port'));
 });
 
+app.get('/api/shows', function(req, res, next){
+  var query = Show.find();
+  if (req.query.genre) {
+    query.where({ genre: req.query.genre });
+  } else if (req.query.alphabet) {
+    query.where({ name: new RegExp('^' + '[' + req.query.alphabet + ']', 'i')});
+  } else {
+    query.limit(12);
+  }
+  query.exec(function(err, shows){
+    if (err) return next(err);
+    res.send(shows);
+  });
+});
+
+app.get('/api/shows/:id', function(req, res, next){
+  Show.findById(req.params.id, function(err, show){
+    if (err) return next(err);
+    res.send(show);
+  });
+});
+
+app.use(function(err, res, next){
+  console.error(err.stack);
+  res.send(500, { message: err.message });
+});
 /*// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
