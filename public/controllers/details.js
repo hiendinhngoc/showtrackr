ee7e3f4dbcea422315e855fa5c01965cf6d5d92f
hiendinhngoc <hiendn@bitnemo.vn>
2016-01-27 22:16:15 +0700

    :tv: complete project

--- public/controllers/details.js
+++ public/controllers/details.js
@@ -1,25 +1,30 @@
 angular.module('MyApp')
-	.controller('DetailCtrl', ['$scope', '$rootScope', '$routeParams', 'Show', 'Subscription',
-		function($scope, $rootScope, $routeParams, Show, Subscription) {
-			Show.get({
-				_id: $routeParams.id
-			}, function(show) {
-				$scope.show = show;
+  .controller('DetailCtrl', function($scope, $rootScope, $routeParams, Show, Subscription) {
+      Show.get({ _id: $routeParams.id }, function(show) {
+        $scope.show = show;
 
-				$scope.isSubscribed = function() {
-					return $scope.show.subsribers.indexOf($rootScope.currentUser._id) !== -1;
-				};
+        $scope.isSubscribed = function() {
+          return $scope.show.subscribers.indexOf($rootScope.currentUser._id) !== -1;
+        };
 
-				$scope.subsribe = function() {
-					Subscription.subsribe(show).success(function() {
-						var index = $scope.show.subsribers.indexOf($rootScope.currentUser._id);
-						$scope.show.subsribers.splice(index, 1);
-					});
-				};
+        $scope.subscribe = function() {
+          Subscription.subscribe(show).success(function() {
+            $scope.show.subscribers.push($rootScope.currentUser._id);
+          });
+        };
 
-				$scope.nextEpisode = show.episodes.filter(function(episode) {
-					return new Date(episode.firstAired) > new Date();
-				})[0];
-			});
-		}
-	]);
+        $scope.unsubscribe = function() {
+          Subscription.unsubscribe(show).success(function() {
+            var index = $scope.show.subscribers.indexOf($rootScope.currentUser._id);
+            $scope.show.subscribers.splice(index, 1);
+          });
+        };
+
+        $scope.nextEpisode = show.episodes.filter(function(episode) {
+          return new Date(episode.firstAired) > new Date();
+        })[0];
+      });
+    });
+
+    Status API Training Shop Blog About Pricing 
+
